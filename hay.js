5d5fe247dc221cd58b3e9e1fa5d6f55492a8dde6
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-06-04 16:30:19 +0800

    first commit

---
+++ hay.js
@@ -0,0 +1,6 @@
+function soal1() {
+    let a = 5;
+    let b = 10;
+
+    return  a - b;
+}
---
+++ uhuy.js
@@ -0,0 +1,5 @@
+function changeImage()
+{
+    let img = document.getElementById('image');
+    img.src = '62f07b64afff9.jpg'
+} 
